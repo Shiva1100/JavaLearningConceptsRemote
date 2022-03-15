b0821d809bd6dc44729b2bf0d1fab5a3e0204adb
abh <vabhilashkumar98@gmail.com>
2022-03-15 16:53:55 +0000

    Abhilash : 15-03-2022 Added the New Method Total Number of Students

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,9 +2,9 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("This is working fine");
-		
+
 		// creating the object name 
 		MainClass mc = new MainClass();
 		// calling the method : objectName.method();
@@ -18,42 +18,35 @@ public class MainClass {
 		System.out.println("result of String is "+res);
 		int res1=numOfMonths();
 		System.out.println("Total number of Months "+res1);
-		
+		String res2=mc.collage(1, "Abhilash");
+		System.out.println("The student details"+res2);
+		int res3=mc.studentTotal(55, 42);
+		System.out.println("The Total Number of students in the ClassA and ClassB = "+res3);
 	}
-	
+
+	public String collage(int sno,String name) {
+		String details=sno+name;
+		return details;
+	}
 	public static int numOfMonths() {
+
 		int months=12;
 		return months;
-		
-		
-		
-		
 	}
 	public String revStrig(String str) {
-		
 		char ch; String revstr="";
-		
 		for (int i=0;i<str.length();i++)
 		{
 			ch=str.charAt(i);
 			revstr=ch+revstr;
-		
-	}
+		}
 		System.out.println("The reverse String is :"+revstr);
 		return revstr;
-		
-		
-		
-		
-		
 	}
-	
 	public String subOfString(String str, int a) {
-		
 		String subsStr = str.substring(a,5);
 		return subsStr;
 	}
-
 	public void splitOfStr(String str) {
 		String[] ar1 = str.split(" ");
 		int lengthOfAr1 = ar1.length;
@@ -61,9 +54,14 @@ public class MainClass {
 			String eachItem = ar1[i];
 			System.out.println("The Invidiual items "+eachItem);
 		}
-		
-
-		
 	}
+public int studentTotal(int classA,int classB) {
+	int Totl=classA+classB;
+	return Totl;
 	
+	
+	
+	
+}
+
 }
